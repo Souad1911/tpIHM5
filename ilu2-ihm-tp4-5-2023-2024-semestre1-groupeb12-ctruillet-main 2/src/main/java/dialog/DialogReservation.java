bbd1aef5858e238ff83f6536886cf4a48e1f6641
ALGER <alger@MacBook-Air-de-ALGER.local>
2023-10-20 13:02:53 +0200

    TPIHM

--- ilu2-ihm-tp4-5-2023-2024-semestre1-groupeb12-ctruillet-main 2/src/main/java/dialog/DialogReservation.java
+++ ilu2-ihm-tp4-5-2023-2024-semestre1-groupeb12-ctruillet-main 2/src/main/java/dialog/DialogReservation.java
@@ -7,7 +7,11 @@ package dialog;
 import interface_noyau_fonctionnel.InterfaceNoyauFonctionnel;
 import java.awt.EventQueue;
 import java.time.LocalDate;
+import javax.swing.JOptionPane;
 import presentation.FrameReservation;
+
+
+      
 
 public class DialogReservation {
 
@@ -24,36 +28,55 @@ public class DialogReservation {
         frameReservation.setDialog(this);
         frameReservation.setVisible(true);
     }
+      private LocalDate date;
+	private String time;
+	private String numPersons;
+	private int numTable;
 
     public void handleDateSelectedEvent(LocalDate date) {
         //TODO
+        this.date = date;
+        this.frameReservation.enableTime();
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public void handleTimeSelectedEvent(String time) {
         //TODO
+        this.time = time;
+		this.frameReservation.enableNumofPersons();
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public void handleNumOfPersonsSelectedEvent(int nbPersons) {
         //TODO
+        this.numPersons = numPersons;
+	this.frameReservation.enableTableMap();
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public void handleTableSelectedEvent(int numTable) {
         //TODO
+        	this.numTable = numTable;
+		System.out.println(numTable);
+		this.frameReservation.enableValidationInformation(this.date, this.time, this.numPersons, numTable);
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public void handleCancelEvent() {
         //TODO
-        throw new UnsupportedOperationException("Not implemented yet");
+     throw new UnsupportedOperationException("Not implemented yet");
     }
 
-    public void handleValidationEvent() {
+    //public void handleValidationEvent() {
         //TODO
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+        //throw new UnsupportedOperationException("Not implemented yet");
+   // }
+    public void handleValidateEvent() {
+		// Display confirmation window using JOptionPane
+		this.frameReservation.enableValidationInformation(this.date, this.time, this.numPersons, this.numTable);
+		JOptionPane.showMessageDialog(null, "Confirmation de réservation\n\n Réservation confirmée le " + this.date
+				+ " à " + this.time + " pour " + this.numPersons + " personnes à la table " + this.numTable + ".");
+	}
 
     public static void main(String[] args) {
         DialogReservation dialog = new DialogReservation(new InterfaceNoyauFonctionnel());
---
+++ ilu2-ihm-tp4-5-2023-2024-semestre1-groupeb12-ctruillet-main 2/src/main/java/presentation/FrameReservation.java
@@ -0,0 +1,274 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package presentation;
+
+import com.github.lgooddatepicker.optionalusertools.DateChangeListener;
+import com.github.lgooddatepicker.zinternaltools.DateChangeEvent;
+import dialog.DialogReservation;
+import java.time.LocalDate;
+
+@SuppressWarnings("serial")
+public class FrameReservation extends javax.swing.JFrame {
+
+    private DialogReservation dialog;
+
+    public FrameReservation() {
+    }
+
+    public void initFrame() {
+        initComponents();
+    }
+
+    public void setDialog(DialogReservation dialog) {
+        this.dialog = dialog;
+    }
+
+    // /!\ /!\ /!\ ATTENTION /!\ /!\ /!\
+    // ce code est auto généré et ne doit PAS être modifié
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        menuBar1 = new java.awt.MenuBar();
+        menu1 = new java.awt.Menu();
+        menu2 = new java.awt.Menu();
+        menuBar2 = new java.awt.MenuBar();
+        menu3 = new java.awt.Menu();
+        menu4 = new java.awt.Menu();
+        dateTimePanel = new javax.swing.JPanel();
+        datePicker = new com.github.lgooddatepicker.components.DatePicker();
+        datePicker.addDateChangeListener(new DateChangeListener(){
+            public void dateChanged(DateChangeEvent dateEvent) {
+                datePickerDateChanged(dateEvent);
+            }
+        });
+        jLabelDate = new javax.swing.JLabel();
+        jComboBoxHeure = new javax.swing.JComboBox<>();
+        jLabelHeure = new javax.swing.JLabel();
+        nbPersonsPanel = new javax.swing.JPanel();
+        jLabelNBpersonnes = new javax.swing.JLabel();
+        jComboBoxNBPersonnes = new javax.swing.JComboBox<>();
+        pickTablePanel = new javax.swing.JPanel();
+        tablesImage = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+        jButtonAnnuler = new javax.swing.JButton();
+        jButtonValider = new javax.swing.JButton();
+
+        menu1.setLabel("File");
+        menuBar1.add(menu1);
+
+        menu2.setLabel("Edit");
+        menuBar1.add(menu2);
+
+        menu3.setLabel("File");
+        menuBar2.add(menu3);
+
+        menu4.setLabel("Edit");
+        menuBar2.add(menu4);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        dateTimePanel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jLabelDate.setText("1.Choisir la date ");
+
+        jComboBoxHeure.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "11H30", "12H00", "12H30", "13H00", "13H30", " " }));
+
+        jLabelHeure.setText("2.Choisir l'heure");
+
+        javax.swing.GroupLayout dateTimePanelLayout = new javax.swing.GroupLayout(dateTimePanel);
+        dateTimePanel.setLayout(dateTimePanelLayout);
+        dateTimePanelLayout.setHorizontalGroup(
+            dateTimePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, dateTimePanelLayout.createSequentialGroup()
+                .addGroup(dateTimePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(dateTimePanelLayout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(jLabelDate, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(dateTimePanelLayout.createSequentialGroup()
+                        .addGap(21, 21, 21)
+                        .addComponent(datePicker, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(dateTimePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jComboBoxHeure, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelHeure))
+                .addGap(78, 78, 78))
+        );
+        dateTimePanelLayout.setVerticalGroup(
+            dateTimePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, dateTimePanelLayout.createSequentialGroup()
+                .addContainerGap(30, Short.MAX_VALUE)
+                .addGroup(dateTimePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelDate)
+                    .addComponent(jLabelHeure))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(dateTimePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(datePicker, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBoxHeure, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(51, 51, 51))
+        );
+
+        datePicker.getAccessibleContext().setAccessibleName("");
+        datePicker.getAccessibleContext().setAccessibleDescription("");
+
+        nbPersonsPanel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jLabelNBpersonnes.setText("3.Choisir le nombres de personnes ");
+
+        jComboBoxNBPersonnes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { " ", "2", "3", "4", "5", "6", "7", "8", " " }));
+
+        javax.swing.GroupLayout nbPersonsPanelLayout = new javax.swing.GroupLayout(nbPersonsPanel);
+        nbPersonsPanel.setLayout(nbPersonsPanelLayout);
+        nbPersonsPanelLayout.setHorizontalGroup(
+            nbPersonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nbPersonsPanelLayout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addGroup(nbPersonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(nbPersonsPanelLayout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(jComboBoxNBPersonnes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabelNBpersonnes, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        nbPersonsPanelLayout.setVerticalGroup(
+            nbPersonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nbPersonsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabelNBpersonnes)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jComboBoxNBPersonnes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(56, Short.MAX_VALUE))
+        );
+
+        pickTablePanel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        tablesImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Plan_tables.jpg"))); // NOI18N
+
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Table 2", "Table 3 ", "Table 4", "Table 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
+
+        javax.swing.GroupLayout pickTablePanelLayout = new javax.swing.GroupLayout(pickTablePanel);
+        pickTablePanel.setLayout(pickTablePanelLayout);
+        pickTablePanelLayout.setHorizontalGroup(
+            pickTablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pickTablePanelLayout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(tablesImage, javax.swing.GroupLayout.PREFERRED_SIZE, 297, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(59, 59, 59)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(45, Short.MAX_VALUE))
+        );
+        pickTablePanelLayout.setVerticalGroup(
+            pickTablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pickTablePanelLayout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(tablesImage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(pickTablePanelLayout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jButtonAnnuler.setText("Annuler");
+
+        jButtonValider.setText("Valider");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(dateTimePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(nbPersonsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(pickTablePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButtonValider)
+                .addGap(18, 18, 18)
+                .addComponent(jButtonAnnuler)
+                .addGap(35, 35, 35))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(dateTimePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(nbPersonsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(pickTablePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonAnnuler)
+                    .addComponent(jButtonValider))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public void datePickerDateChanged(DateChangeEvent dateEvent) {
+        //TODO
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.github.lgooddatepicker.components.DatePicker datePicker;
+    private javax.swing.JPanel dateTimePanel;
+    private javax.swing.JButton jButtonAnnuler;
+    private javax.swing.JButton jButtonValider;
+    private javax.swing.JComboBox<String> jComboBoxHeure;
+    private javax.swing.JComboBox<String> jComboBoxNBPersonnes;
+    private javax.swing.JLabel jLabelDate;
+    private javax.swing.JLabel jLabelHeure;
+    private javax.swing.JLabel jLabelNBpersonnes;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private java.awt.Menu menu1;
+    private java.awt.Menu menu2;
+    private java.awt.Menu menu3;
+    private java.awt.Menu menu4;
+    private java.awt.MenuBar menuBar1;
+    private java.awt.MenuBar menuBar2;
+    private javax.swing.JPanel nbPersonsPanel;
+    private javax.swing.JPanel pickTablePanel;
+    private javax.swing.JLabel tablesImage;
+    // End of variables declaration//GEN-END:variables
+
+    //public void enableValidationInformation(String date, String time, String numPersons, int numTable) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    //}
+    public void enableValidationInformation(LocalDate date1, String date, String time, int numTable) {
+		this.jButtonValider.setEnabled(true);
+	}
+
+    public void enableTableMap() {
+        this.jList1.setEnabled(true);
+	//this.lblTableMap.setEnabled(true);
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void enableNumofPersons() {
+        this.jComboBoxNBPersonnes.setEnabled(true);
+	this.jLabelNBpersonnes.setEnabled(true);
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void enableTime() {
+        this.jComboBoxHeure.setEnabled(true);
+	this.jLabelHeure.setEnabled(true);
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
